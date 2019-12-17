e3fd78440162f58922de7e35f2bb1cefd32bee46
Júlio César <julio.rodrigues@zahi.com.br>
2019-12-17 17:03:09 -0300

    remove and get typegeneric

--- SlimFormaturas.Api/Controllers/TypeController.cs
+++ SlimFormaturas.Api/Controllers/TypeController.cs
@@ -17,19 +17,41 @@ namespace SlimFormaturas.Api.Controllers
             _typeGenericService = typeGenericService;
         }
 
-        // GET: api/Type
-        //[HttpGet("Address")]
+        //[CustomAuthorize.ClaimsAuthorize("Graduate", "Incluir")]
+        [HttpPost]
+        public async Task<ActionResult<string>> Post(TypeGeneric typeGeneric) {
+            _ = await _typeGenericService.Insert(typeGeneric);
+            return Response(typeGeneric.TypeGenericId);
+        }
+
+        //[CustomAuthorize.ClaimsAuthorize("Graduate", "Editar")]
+        [HttpPut]
+        public async Task<ActionResult<string>> Put(TypeGeneric typeGeneric) {
+            _ = await _typeGenericService.Update(typeGeneric);
+            return Response(typeGeneric.TypeGenericId);
+        }
+
+        //[CustomAuthorize.ClaimsAuthorize("Graduate", "Excluir")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id) {
+            await _typeGenericService.Delete(id);
+            return Response();
+        }
+
+        // GET api/Graduate
+        /// <summary>
+        /// Get all graduates
+        /// </summary>
+        /// <remarks>This API will get the values.</remarks>
+        /// 
         [HttpGet]
         public async Task<ActionResult> Get() {
             return Response(await _typeGenericService.Get());
         }
 
-        //[HttpPost("Address")]
-        [HttpPost]
-        public async Task<ActionResult<TypeGeneric>> Post(TypeGeneric typeGeneric) {
-            //typeGeneric.Localization = Locate.Address;
-            await _typeGenericService.Insert(typeGeneric);
-            return Response(typeGeneric.TypeGenericId);
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(string id) {
+            return Response(await _typeGenericService.Get(id));
         }
 
     }
