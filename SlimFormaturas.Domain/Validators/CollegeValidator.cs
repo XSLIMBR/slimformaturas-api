671e6b919ed341f79d8d5268e70e74e5f29caa88
Júlio César Oliveira Rodrigues <senhorodrigues@live.com>
2020-01-05 21:37:49 -0300

    College

---
+++ SlimFormaturas.Domain/Validators/CollegeValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using SlimFormaturas.Domain.Entities;
+
+namespace SlimFormaturas.Domain.Validators {
+    public class CollegeValidator : AbstractValidator<College> {
+        public CollegeValidator() {
+            RuleFor(c => c)
+                .NotNull()
+                .OnAnyFailure(x => throw new ArgumentNullException("O objeto não foi encontrado!"));
+            RuleFor(c => c.CorporateName)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+            RuleFor(c => c.FantasyName)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+            RuleFor(c => c.CNPJ)
+                .NotEmpty()
+                .NotNull()
+                .Length(14);
+            RuleFor(c => c.Bank)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+            RuleFor(c => c.Agency)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+            RuleFor(c => c.CheckingAccount)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+            RuleFor(c => c.Site)
+                .NotEmpty()
+                .NotNull()
+                .Length(1, 50);
+        }
+    }
+}
