31f5ecaafa862a0a7f39aeed5b7de34cbf7a974c
JulioRodrigues <senhorodrigues@live.com>
2020-04-04 07:21:34 -0300

    graduat DTO

---
+++ SlimFormaturas.Domain/Dto/GraduateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimFormaturas.Domain.Dto {
+    public class GraduateDto {
+        public string GraduateId { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string DadName { get; set; }
+        public string MotherName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+        public string Bank { get; set; }
+        public string Agency { get; set; }
+        public string CheckingAccount { get; set; }
+    }
+}
