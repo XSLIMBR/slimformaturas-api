fc1a3ca1489b07d16d9e13d88a87518193bd0ddf
fabio said <fabiosaid635@gmail.com>
2020-04-12 18:21:28 -0300

    Entitie : University

--- SlimFormaturas.Api/Controllers/UniversityController.cs
+++ SlimFormaturas.Api/Controllers/UniversityController.cs
@@ -5,6 +5,8 @@ using SlimFormaturas.Domain.Interfaces.Service;
 using SlimFormaturas.Domain.Notifications;
 using System.Threading.Tasks;
 using AutoMapper;
+using System.Collections.Generic;
+using SlimFormaturas.Domain.Dto.University;
 
 namespace SlimFormaturas.Api.Controllers
 {
@@ -27,17 +29,31 @@ namespace SlimFormaturas.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(UniversityDto universityDto)
+        public async Task<IActionResult> Post([FromBody] UniversityForCreationDto universityDto)
         {
             var university = _mapper.Map<University>(universityDto);
             return Response(await _universityService.Insert(university)); 
         }
 
         [HttpPut]
-        public async Task<IActionResult> Put(UniversityDto universityDto)
+        public async Task<IActionResult> Put([FromBody] UniversityForCreationDto universityDto)
         {
             var university = _mapper.Map<University>(universityDto);
-            return Response(await _universityService.Insert(university));
+            return Response(await _universityService.Update(university));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UniversityDto>> Get()
+        {
+            var university = _mapper.Map<IList<UniversityDto>>(await _universityService.Get());
+            return Response(university);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UniversityDto>> Get(string id)
+        {
+            var university = _mapper.Map<UniversityDto>(await _universityService.Get(id));
+            return Response(university);
         }
     }
 }
---
+++ SlimFormaturas.Domain/Dto/University/UniversityForCreationDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimFormaturas.Domain.Dto.University
+{
+    public class UniversityForCreationDto
+    {
+        public string Name { get; set; }
+    }
+}
