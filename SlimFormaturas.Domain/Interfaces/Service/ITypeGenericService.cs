5748ced9a168807d77be85600fdd2358fa2da43c
Júlio César Oliveira Rodrigues <senhorodrigues@live.com>
2020-06-16 13:36:13 -0300

    typedinamic

--- SlimFormaturas.Domain/Interfaces/Service/ITypeGenericService.cs
+++ SlimFormaturas.Domain/Interfaces/Service/ITypeGenericService.cs
@@ -1,10 +1,11 @@
-﻿using SlimFormaturas.Domain.Entities;
+﻿using SlimFormaturas.Domain.Dto.TypeGeneric;
+using SlimFormaturas.Domain.Entities;
 using SlimFormaturas.Domain.Interfaces.Service;
 using System.Threading.Tasks;
 
 namespace SlimFormaturas.Domain.Interfaces.Service {
     public interface ITypeGenericService : IService<TypeGeneric> {
         Task<TypeGeneric> Insert(TypeGeneric obj);
-        Task<TypeGeneric> Update(TypeGeneric obj);
+        Task<TypeGeneric> Update(TypeGenericDto obj);
     }
 }
