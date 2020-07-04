9a078eb50ed250a03c10fe13cd16037c406401d9
julio.rodrigues <senhorodrigues@live.com>
2020-07-04 11:52:39 -0300

    removed cord credentials

--- SlimFormaturas.Api/Startup.cs
+++ SlimFormaturas.Api/Startup.cs
@@ -41,8 +41,7 @@ namespace SlimFormaturas.Api {
                 options.AddPolicy("CorsPolicy",
                     builder => builder.AllowAnyOrigin()
                     .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
+                    .AllowAnyHeader());
             });
 
 
