b7438b2ad439ba8b8e0ab0e353c463d7af4f4b1c
julio.rodrigues <senhorodrigues@live.com>
2020-07-04 12:36:55 -0300

    retirado á necessidade de login dos telefones

--- SlimFormaturas.Api/Controllers/PhoneController.cs
+++ SlimFormaturas.Api/Controllers/PhoneController.cs
@@ -11,7 +11,7 @@ using SlimFormaturas.Domain.Dto.Phone;
 
 namespace SlimFormaturas.Api.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PhoneController : ApiController {
