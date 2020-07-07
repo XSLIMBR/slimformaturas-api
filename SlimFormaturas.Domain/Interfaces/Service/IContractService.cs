a369516f485362182cf28ce9ca490ef2ad951089
fabio said <fabiosaid635@gmail.com>
2020-07-06 21:31:43 -0300

    Entity: Contract

---
+++ SlimFormaturas.Domain/Interfaces/Service/IContractService.cs
@@ -0,0 +1,13 @@
+﻿using SlimFormaturas.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimFormaturas.Domain.Interfaces.Service
+{
+    public interface IContractService : IService<Contract>
+    {
+        Task<Contract> Insert(Contract obj);
+    }
+}
