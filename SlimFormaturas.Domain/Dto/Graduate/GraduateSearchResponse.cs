4d185f72cf49e5069529abc3470438aea924b259
julio.rodrigues <senhorodrigues@live.com>
2020-07-23 23:51:28 -0300

    mais parametros de retorno

--- SlimFormaturas.Domain/Dto/Graduate/GraduateSearchResponse.cs
+++ SlimFormaturas.Domain/Dto/Graduate/GraduateSearchResponse.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace SlimFormaturas.Domain.Dto.Graduate {
     public class GraduateSearchResponse {
+        public string GraduateId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Photo { get; set; }
     }
 }
